26d1099b1007e287d8a89bd5ac4dd3d509d3795b
Matheusvicentesn <matheusvicentesj5@hotmail.com>
2022-10-21 20:58:18 -0300

    barra v3

--- assets/script/script.js
+++ assets/script/script.js
@@ -79,36 +79,36 @@
 //   }
 // }
 
-// // dark mode
-// function darkMode() {
-//   const element = document.body;
-//   element.classList.toggle("dark-mode");
-// }
+// dark mode
+function darkMode() {
+  const element = document.body;
+  element.classList.toggle("dark-mode");
+}
 
-const slider = document.querySelector(".parent");
-let mouseDown = false;
-let startX, scrollLeft;
+// const slider = document.querySelector(".parent");
+// let mouseDown = false;
+// let startX, scrollLeft;
 
-let startDragging = function (e) {
-  mouseDown = true;
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
-};
-let stopDragging = function (event) {
-  mouseDown = false;
-};
+// let startDragging = function (e) {
+//   mouseDown = true;
+//   startX = e.pageX - slider.offsetLeft;
+//   scrollLeft = slider.scrollLeft;
+// };
+// let stopDragging = function (event) {
+//   mouseDown = false;
+// };
 
-slider.addEventListener("mousemove", (e) => {
-  e.preventDefault();
-  if (!mouseDown) {
-    return;
-  }
-  const x = e.pageX - slider.offsetLeft;
-  const scroll = x - startX;
-  slider.scrollLeft = scrollLeft - scroll;
-});
+// slider.addEventListener("mousemove", (e) => {
+//   e.preventDefault();
+//   if (!mouseDown) {
+//     return;
+//   }
+//   const x = e.pageX - slider.offsetLeft;
+//   const scroll = x - startX;
+//   slider.scrollLeft = scrollLeft - scroll;
+// });
 
-// Add the event listeners
-slider.addEventListener("mousedown", startDragging, false);
-slider.addEventListener("mouseup", stopDragging, false);
-slider.addEventListener("mouseleave", stopDragging, false);
+// // Add the event listeners
+// slider.addEventListener("mousedown", startDragging, false);
+// slider.addEventListener("mouseup", stopDragging, false);
+// slider.addEventListener("mouseleave", stopDragging, false);
